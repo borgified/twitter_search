eacc3ae654ff6456cbd5f819803bcad055e60d57
borgified <borgified@gmail.com>
2012-08-08 21:06:24 -0700

    both alphabet and numbers are now together in one alphanumeric array

--- name_gen.pl
+++ name_gen.pl
@@ -2,32 +2,22 @@
 use strict;
 use warnings;
 
-my @alphabet=("a".."b");
-my @number=("0".."2");
+my @alphabet=("a".."z");
+my @number=("0".."9");
+
+my @alphanumeric = (@alphabet,@number);
+
+my @word;
 
 # all letters
-foreach my $item(@alphabet){
-	foreach  my $item2(@alphabet){
-		foreach  my $item3(@alphabet){
+foreach my $item(@alphanumeric){
+	foreach  my $item2(@alphanumeric){
+		foreach  my $item3(@alphanumeric){
 			push(@word,"$item$item2$item3");
 		}
 	}
 }
 foreach my $line(@word){
-	chomp($line);
 	print "$line\n";
 }
-# all numbers
-foreach my $num(@number){
-	foreach  my $num2(@number){
-		foreach  my $num3(@number){
-			print "$num$num2$num3\n";
-			#push(@word,"$item$item2$item3");
-		}
-	}
-}
-# mix letter/numbers
-#check if exists in hash replace first 1st, 2nd, 3rd, 
-#if exists, next 
-#else enter into hash
 
