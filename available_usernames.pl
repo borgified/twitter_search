390bd381aa14c3b5cd00d1416c497f6e0f842fee
borgified <borgified@gmail.com>
2012-08-16 23:29:54 -0700

    removed the db stuff, we arent gonna use dbs anymore added a ton of comments to the code

--- available_usernames.pl
+++ available_usernames.pl
@@ -4,7 +4,6 @@ use warnings;
 use strict;
 use Net::Twitter;
 use Scalar::Util 'blessed';
-use DBI;
 use Algorithm::Combinatorics qw(:all);
 
 my %config = do '/secret/twitter.config';
@@ -13,17 +12,6 @@ my $consumer_key		= $config{consumer_key};
 my $consumer_secret		= $config{consumer_secret};
 my $token			= $config{token};
 my $token_secret		= $config{token_secret};
-
-
-#my $my_cnf = '/secret/my_cnf.cnf';
-
-#my $dbh = DBI->connect("DBI:mysql:"
-#                        . ";mysql_read_default_file=$my_cnf"
-#                        .';mysql_read_default_group=twitter_search',
-#                        undef,
-#                        undef
-#                        ) or die "something went wrong ($DBI::errstr)";
-
 
 # As of 13-Aug-2010, Twitter requires OAuth for authenticated requests
 my $nt = Net::Twitter->new(
@@ -34,13 +22,27 @@ my $nt = Net::Twitter->new(
     access_token_secret => $token_secret,
 );
 
+#this is to hold the results that come back from querying twitter
 my $arrayref;
 
+#$ar contains array ref to all the different combinations of usernames possible
+#valid chars and length of username are all configured within &gen_names
+#the array looks like this: ('aaa,aab,aac,...','baa,bab,bac,...',...) where each
+#element in the array is a comma separated string composed of a 100 twitter handles
+#to test. (lookup_users allows max 100 user queries)
 my $ar=&gen_names;
-#my $names=shift(@$ar);
+
+#the @ARGV given determines which set of 100 twitter handles to use... ie. $arg=0 is the
+#first set of 100 user handles. (used as array index)
 my $arg=shift(@ARGV);
 my $names=$$ar[$arg];
 
+#lookup_users only returns back valid users, not missing ones, so we create a hash
+#using the list of 100 twitter handles that was used as input to lookup_users.
+#then fill in each key's value with the user's twitter id according to what lookup_users
+#returns. those hash keys not having a corresponding value means lookup_users was not able
+#to get their id, so those keys must be either unclaimed accounts or suspended ones or
+#just somehow invalid (these accounts are manually placed in the "weird" file)
 my %results_hash;
 my @names = split(/,/,$names);
 foreach my $name (@names){
@@ -65,6 +67,9 @@ if ( my $err = $@ ) {
 open(OUTPUT,">>results.txt") or die $!;
 print OUTPUT "===========================\n";
 
+#this loop stores the result of lookup_users into a text file, just a couple fields per
+#account. also stores the found twitter id for each user into the %results_hash we prepared
+#earlier.
 foreach my $hash (@$arrayref){
 
 #	foreach my $key (keys %$hash){
@@ -80,6 +85,8 @@ foreach my $hash (@$arrayref){
 
 }
 
+#print back out the filled out %results_hash so we can see which accounts (hash keys)
+#dont have a value (twitter id) attached to it.
 my $count=0;
 foreach my $screen_name (sort keys %results_hash){
 	if($results_hash{$screen_name} eq ''){
@@ -96,28 +103,21 @@ foreach my $item (sort keys %$status){
 	print "$item $$status{$item};\n" unless($item eq 'photos');
 }
 
-### verbose output for testing gen_names
-#my $ar=&gen_names;
-#my $x=0;
-#foreach my $item (@$ar){
-#	print "$item\n";
-#	$x++;
-#	print "$x ====================\n";
-#}
-
-
 ### generate usernames
 #input: none
 #output: array of comma delimited usernames strings of 100 users each
 sub gen_names{
 
+#modify the below 3 variables to alter the pool of allowed twitter username chars
 	my @alphabet=("a".."z");
 	my @number=("0".."9");
 	my @other=("_");
+
 	my @results;
 
 	my @alphanumeric = (@alphabet,@number,@other);
 
+#modify this variable if you want to test longer usernames
 	my $length = 3;
 
 	my $iter = variations_with_repetition(\@alphanumeric,$length);
